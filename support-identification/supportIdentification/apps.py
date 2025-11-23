99daa5dcf8573a08c33f3e354e0be571abd2d2fd
Murillo Faria <murillofaria.contato@gmail.com>
2025-11-23 13:05:22 -0300

    Revert "Criação README"

--- support-identification/supportIdentification/apps.py
+++ support-identification/supportIdentification/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class SupportidentificationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'supportIdentification'
-
-    def ready(self):
-        import supportIdentification.signals
